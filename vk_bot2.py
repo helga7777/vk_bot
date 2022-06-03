00bd97e8bdfb7412c5be853d4b5e87a55bc81dc5
helga7777 <66511185+helga7777@users.noreply.github.com>
2022-06-03 11:20:46 +0300

    Update vk_bot2.py

--- vk_bot2.py
+++ vk_bot2.py
@@ -7,8 +7,6 @@ from io import BytesIO
 from vk_api.upload import VkUpload
 from vk_api.utils import get_random_id
 from vk_user_info import VkUser
-from vk_button import vk_add_button
-
 
 
 token = ''
@@ -31,9 +29,8 @@ def vk_bot_id_user():
     messages = vk.method('messages.getConversations', value)
     if messages['count'] >= 1:
         user_id = messages['items'][0]['last_message']['from_id']
-        # получили ид пользователя, который начал общение
         return user_id
-    # вернул мой id vk
+
 
 def upload_photo(upload, url):
     img = requests.get(url).content
@@ -42,11 +39,6 @@ def upload_photo(upload, url):
     owner_id = response['owner_id']
     photo_id = response['id']
     access_key = response['access_key']
-    # print(owner_id, photo_id, access_key)
-    # вернул
-    # -213472651    457239152    7dc8d1f4a3986c418f
-    # -213472651    457239153    0e7fc73c938545faaf
-    # -213472651    457239154    8a9b971bb74e88662f
     return owner_id, photo_id, access_key
 
 def send_photo(vk, peer_id, owner_id, photo_id, access_key):
@@ -54,7 +46,7 @@ def send_photo(vk, peer_id, owner_id, photo_id, access_key):
     vk.method('messages.send', {'random_id':get_random_id(),'peer_id':peer_id,'attachment':attachment})
 
 
-def vk_bot_message(foto):
+def vk_bot_message(foto, PEER_ID):
         for i in range(3):
             send_photo(vk, PEER_ID, *upload_photo(upload, foto[i]))
 
@@ -66,43 +58,32 @@ def vk_add_button(user_id,user_search):
     def next_user(k):
         fio_str = vk_client.user_id_search(user_search[1][k])
         foto = vk_client.photo_laik(user_search[1][k])
-        # print(foto)
-        # вернул
-        # [{'type': 'photo',
-        #   'photo': 'https://sun9-2.userapi.com/s/v1/if2/QirM-n92xgk5hVp7cNRAUvfLDFb502Jmcf4VJVgEZXUftAXXAfBgvJx7OfN2w4S5Z1O_PokPfYeenw25gjFZ_EyB.jpg?size=1280x719&quality=96&type=album'},
-        #  {'type': 'photo',
-        #   'photo': 'https://sun9-21.userapi.com/s/v1/if1/JqKuDH_naazza7dQkOFcDZUMNkzzyi9N0pDAloGhCwRKtoW1GwRJXHs9h9kMW9oUt-MnKBiN.jpg?size=1024x683&quality=96&type=album'},
-        #  {'type': 'photo', 'photo': 'https://sun9-67.userapi.com/c10569/u94706459/-6/z_42097e00.jpg'}]
-
         foto = [foto[0]['photo'], foto[1]['photo'], foto[2]['photo']]
         fio_name = fio_str[0]
         fio_last = fio_str[1]
         sait = fio_str[2]
-        # next_user = fio_name +  fio_last +  sait +  foto
-        # print(fio_name, fio_last, sait, foto)
-        # вернул
-        # Владимир Ланцов https: // vk.com / id94706459['https://sun9-2.userapi.com/s/v1/if2/QirM-n92xgk5hVp7cNRAUvfLDFb502Jmcf4VJVgEZXUftAXXAfBgvJx7OfN2w4S5Z1O_PokPfYeenw25gjFZ_EyB.jpg?size=1280x719&quality=96&type=album', 'https://sun9-21.userapi.com/s/v1/if1/JqKuDH_naazza7dQkOFcDZUMNkzzyi9N0pDAloGhCwRKtoW1GwRJXHs9h9kMW9oUt-MnKBiN.jpg?size=1024x683&quality=96&type=album', 'https://sun9-67.userapi.com/c10569/u94706459/-6/z_42097e00.jpg']
+
+        #записать в бд fio_name, fio_last, sait,  претендента и foto в отдельную таблицу
+
 
         return fio_name, fio_last, sait, foto
 
-    def create_keyboard(response):
+    def create_keyboard(response=None):
 
         keyboard = VkKeyboard(one_time=True)
-        if response.lower() == ('начать' or 'старт'or 'поиск'):
-            keyboard.add_button('следующий', color=VkKeyboardColor.POSITIVE)
-            keyboard.add_button('Парня', color=VkKeyboardColor.POSITIVE)
-            keyboard.add_button('Всё равно', color=VkKeyboardColor.POSITIVE)
 
-        else:
-            keyboard.add_button('следующий', color=VkKeyboardColor.POSITIVE)
+        keyboard.add_button('следующий', color=VkKeyboardColor.POSITIVE)
+        keyboard.add_line()
+        keyboard.add_button('добавить в избранное', color=VkKeyboardColor.POSITIVE)
+        keyboard.add_line()
+        keyboard.add_button('показать избранное', color=VkKeyboardColor.POSITIVE)
 
         keyboard = keyboard.get_keyboard()
         return keyboard
 
-    # while True:
+
     for event in longpoll.listen():
 
-    # event = longpoll.listen()
         if event.type == VkEventType.MESSAGE_NEW:
             print('Сообщение пришло в: ' + str(datetime.strftime(datetime.now(), "%H:%M:%S")))
             print('Текст сообщения: ' + str(event.text))
@@ -118,20 +99,35 @@ def vk_add_button(user_id,user_search):
                                'random_id': get_random_id(),
                                'keyboard': keyboard})
             if response == 'следующий':
-                #
                 new_user = next_user(k)
-                # print(new_user[0])
                 fio_name = new_user[0]
                 fio_last = new_user[1]
                 sait = new_user[2]
-                # print(sait)
                 foto = new_user[3]
                 vk.method('messages.send',
                             {'user_id': event.user_id, 'message': f'Имя: {fio_name}\n Фамилия: {fio_last}\n Страничка:{sait}\n',
                                'random_id': get_random_id(),
                                'keyboard': keyboard})
-                vk_bot_message(foto)
+                vk_bot_message(foto,user_id)
                 k += 1
+            if response == 'добавить в избранное':
+                new_user = next_user(k)
+                fio_name = new_user[0]
+                fio_last = new_user[1]
+                vk.method('messages.send',
+                          {'user_id': event.user_id, 'message': f' {fio_name} {fio_last} добавлен/а в избранное',
+                           'random_id': get_random_id(),
+                           'keyboard': keyboard})
+
+            if response == 'показать избранное':
+                vk.method('messages.send',
+                          {'user_id': event.user_id, 'message': 'Ваше избранное:',
+                           'random_id': get_random_id(),
+                           'keyboard': keyboard})
+
+
+
+
 
         print('--------------------------------------------')
 
@@ -142,5 +138,4 @@ token = ''
 vk = vk_api.VkApi(token=token)
 vk._auth_token()
 upload = VkUpload(vk)
-PEER_ID = 
-# URL = '...'
+
